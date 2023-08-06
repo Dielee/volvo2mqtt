7d7f663b7e2d4c55ae5a4cf343280a86c29fded2
Arthur van der Peijl <arthur@artned.org>
2023-08-06 13:01:07 +0200

    Make sure bulb warning is communicated

--- src/volvo.py
+++ src/volvo.py
@@ -451,6 +451,6 @@ def parse_api_data(data, sensor_id=None):
     elif sensor_id == "service_warning_trigger":
         return data["serviceWarningTrigger"]["value"] if util.keys_exists(data, "serviceWarningTrigger") else None
     elif sensor_id == "bulb_failure_status":
-        return data["bulbFailure"]["value"] if util.keys_exists(data, "bulbFailure") else None
+        return data["bulbFailure"]["value"] if util.keys_exists(data, "bulbFailure") else 'none'
     else:
         return None
